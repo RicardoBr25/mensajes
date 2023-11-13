113a8943d4367793ff1502cd7ff9af6e19545ca0
ricardobr25 <113959643+RicardoBr25@users.noreply.github.com>
2023-11-13 01:46:50 -0500

    Vieo 7

--- firechat/src/app/components/chat/chat.component.ts
+++ firechat/src/app/components/chat/chat.component.ts
@@ -1,45 +1,18 @@
-import { ChatService } from './../../providers/chat.service';
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: []
+  styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit{
+export class ChatComponent {
 
-  mensaje: string = "";
-  elemento: any;
+  mensaje: string ="";
 
-  constructor( public _cs: ChatService) {
-    this._cs.cargarMensajes()
-    .subscribe(()=> {
-      setTimeout(() => {
-        this.elemento.scrollingTop = this.elemento.scrollHeight;
-      }, 20);
-    });
-   }
+  constructor() {}
 
-   ngOnInit() {
-    this.elemento = document.getElementById('app-mensaje');
-
-   }
-
-
+  
   enviar_mensaje(){
-       console.log( this.mensaje );
-
-       if( this.mensaje.length === 0 ){
-        return;
-       }
-
-       this._cs.agregarMensaje( this.mensaje )
-             .then( ()=>this.mensaje = "")
-             .catch( (err: any)=>console.error('Error al enviar', err));
-
-
-
+    console.log( this.mensaje );
   }
-
 }
